4e469209d4e1a894650820b23a69496ddd58d386
datonno8 <48306555+datonno8@users.noreply.github.com>
2019-06-13 11:00:26 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,104 +1,88 @@
 ﻿using System;
+using System.Collections;
 
-namespace C_Session2
+namespace C_
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Student st = new Student();
-            st.inputInfo();
-            st.showInfo();
-            st.scholarship();
+            bool running = true;
+            int choosed ;
+            mainAction aciton = new mainAction();
+            while (running)
+            {
+                Console.WriteLine("enter number 1-4: ");
+                Console.WriteLine("1: add new contact");
+                Console.WriteLine("2: find contact by name");
+                Console.WriteLine("3: display all contact");
+                Console.WriteLine("4: exit program \n");
+                try {
+                    String choosedS = Console.ReadLine();
+                    choosed = Int32.Parse(choosedS); 
+                } catch(FormatException){
+                    Console.WriteLine("only number in beetwen 1 and 4!");
+                    continue;
+                }
+                
+
+                switch(choosed){
+                    case 1 :
+                        aciton.AddContact();
+                        break;
+                    case 2 :
+                        aciton.findByName();
+                        break;
+                    case 3 :
+                        aciton.displayContact();
+                        break;
+                    case 4 :
+                        running = false;
+                        break;
+                    default :
+                        Console.WriteLine("Unaccpeted input ");
+                        break;
+                }
+            }
+
         }
     }
 
-    class Student
+    class mainAction
     {
-        private String ID;
-        private int avange;
-        private int age;
-        private string classId;
-
-        public Student(string id, int avange, int age, string classId)
-        {
-            ID = id;
-            this.avange = avange;
-            this.age = age;
-            this.classId = classId;
-        }
-
-        public Student()
+        Hashtable contactTable= new Hashtable();
+        public mainAction()
         {
         }
 
-        public string Id
-        {
-            get { return ID; }
-            set { ID = value; }
+        public void AddContact(){
+            Console.WriteLine("enter name: ");
+            String conTactName = Console.ReadLine();
+            Console.WriteLine("enter phoneNumber: ");
+            String phoneNumber = Console.ReadLine();
+            this.contactTable.Add(conTactName,phoneNumber);
         }
-
-        public int Avange
-        {
-            get
-            {return avange;
-            }
-            set  {
-            avange = value;
-        }
-        }
-
-        public int Age
-        {
-            get
+        public void findByName(){
+            String conTactName = Console.ReadLine();
+            if (this.contactTable.ContainsKey(conTactName))
             {
-               return age;
-            }
-            set { age = value; }
-        }
-
-        public string ClassId
-        {
-            get { return classId; }
-            set { classId = value; }
-        }
-
-        public void inputInfo()
-        {
-            Console.WriteLine("nhap vao ID:"+ID);
-            ID = Convert.ToString(Console.ReadLine());
-            
-            Console.WriteLine("nhap vao diem trung binh:"+avange);
-            avange = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("nhap vao tuoi:"+age);
-            age = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("nhap vao lop:"+classId);
-            classId = Convert.ToString(Console.ReadLine());
-
-        }
-
-        public void showInfo()
-        {
-            Console.WriteLine("Thong tin hoc sinh:");
-            Console.WriteLine("ID:"+ID);
-            Console.WriteLine("avange:"+avange);
-            Console.WriteLine("age:"+age);
-            Console.WriteLine("classID:"+classId);
-        }
-
-        public void scholarship()
-        {
-            if (avange>=8.0)
-            {
-                Console.WriteLine("hoc sinh ID:"+ID+" dat hoc bong");
-
+                Console.WriteLine(conTactName + ": "+this.contactTable[conTactName]);
             }
             else
             {
-                Console.WriteLine("hoc sinh khong dat hoc bong");
+                Console.WriteLine("not found this user");
             }
+        }
+        public void displayContact(){
+            ICollection key = this.contactTable.Keys;
+
+            foreach (string k in key)
+            {
+                Console.WriteLine(k + ": " + this.contactTable[k]);
+            }
+
         }
     }
 }
+
+
